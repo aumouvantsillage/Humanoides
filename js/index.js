fbff4402bc7ef05deee105a7641418f8a2e128e3
senshu <guillaume@baierouge.fr>
2017-06-11 19:12:20 +0200

    Added tiles

--- js/index.js
+++ js/index.js
@@ -1,43 +1,109 @@
 
-const game = new Phaser.Game(800, 600, Phaser.AUTO, "", {preload, create, update});
+const BOARD_WIDTH  = 40;
+const BOARD_HEIGHT = 22;
+const CELL_WIDTH   = 24;
+const CELL_HEIGHT  = 24;
+const HUMAN_WIDTH  = 21;
+const HUMAN_HEIGHT = 24;
 
-let player, cursors;
+const SYMBOLS = {
+    "X": "brick",
+    "+": "ladder",
+    "-": "rope",
+    "@": "gift"
+};
+
+const board1 = [
+    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    "X                                      X",
+    "X   +XXXXXXXX                          X",
+    "X   +X      -------+                   X",
+    "X   +X   @  X      +                   X",
+    "X   +XXXXXXXX      +    -----+         X",
+    "X   +              XXXXXX    +         X",
+    "X  +XXXXXXXXXXXXX+ X    X    +         X",
+    "X  +X           X+ X @  X    XXXXXX    X",
+    "X  +X           X+ XXXXXX    X    X    X",
+    "X  +X           X+           X @  X    X",
+    "X  +X           X+    @      XXXXXX    X",
+    "X  +X         XXXXXXXXXXXXXX+          X",
+    "X  +X         X X          X+          X",
+    "X  +X         X X          X+          X",
+    "X  +X         X X          X+  @       X",
+    "X  +X         X X        XXXXXXXXXXX+  X",
+    "X  +X         X X        X X       X+  X",
+    "X  +X         X X        X X       X+  X",
+    "X  +X         X X        X X       X+  X",
+    "X  +X         XXX        XXX       X+  X",
+    "XXX+                                +XXX"
+];
+
+const game = new Phaser.Game(BOARD_WIDTH * CELL_WIDTH, BOARD_HEIGHT * CELL_HEIGHT, Phaser.AUTO, "", {preload, create, update});
+
+let player, cursors, groups;
 
 function preload() {
-    game.load.spritesheet("human", 'assets/human.png', 21, 21);
+    Object.values(SYMBOLS).forEach(name => game.load.image(name, `assets/${name}.png`));
+    game.load.spritesheet("human", "assets/human.png", HUMAN_WIDTH, HUMAN_HEIGHT);
+}
+
+function createBoard(board) {
+    groups = {};
+    Object.values(SYMBOLS).forEach(name => {
+        const g = game.add.group();
+        g.enableBody = true;
+        groups[name] = g;
+    });
+
+    board.forEach((row, i) => {
+        row.split("").forEach((c, j) => {
+            if (c in SYMBOLS) {
+                const g = SYMBOLS[c];
+                const b = groups[g].create(j * CELL_WIDTH, i * CELL_HEIGHT, g);
+                b.body.immovable = true;
+            }
+        });
+    });
 }
 
 function create() {
+    // Start physics system.
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    // Use keyboard input.
     cursors = game.input.keyboard.createCursorKeys();
 
-    player = game.add.sprite(21, game.world.height - 150, "human");
+    // Create player sprite.
+    player = game.add.sprite(CELL_WIDTH, CELL_HEIGHT, "human");
     game.physics.arcade.enable(player);
     player.body.bounce.y = 0.2;
     player.body.gravity.y = 300;
-    player.body.collideWorldBounds = true;
+    player.body.collideWorldBounds = false;
 
-    player.animations.add('left', [0, 1, 2, 3, 4, 5], 10, true);
-    player.animations.add('right', [7, 8, 9, 10, 11, 12], 10, true);
+    player.animations.add("left", [0, 1, 2, 3, 4, 5], 10, true);
+    player.animations.add("right", [7, 8, 9, 10, 11, 12], 10, true);
+
+    createBoard(board1);
 }
 
 function update() {
-    // var hitPlatform = game.physics.arcade.collide(player, platforms);
-
     player.body.velocity.x = 0;
 
-    if (cursors.left.isDown) {
-        player.body.velocity.x = -150;
-        player.animations.play('left');
-    }
-    else if (cursors.right.isDown) {
-        player.body.velocity.x = 150;
-        player.animations.play('right');
+    if (game.physics.arcade.collide(player, groups.brick)) {
+        if (cursors.left.isDown) {
+            player.body.velocity.x = -150;
+            player.animations.play("left");
+        }
+        else if (cursors.right.isDown) {
+            player.body.velocity.x = 150;
+            player.animations.play("right");
+        }
+        else {
+            player.animations.stop();
+            player.frame = 6;
+        }
     }
     else {
-        player.animations.stop();
-        player.frame = 6;
+        player.frame = 13;
     }
-
 }
